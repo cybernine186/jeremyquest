dea6ff9f2c7255f8d8f0b52c4767d9433d2fd541
quido <hiquido@gmail.com>
2020-08-27 01:34:26 -0400

    Remove Include

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 
-include_once("displayfunctions.php");
-
 /*******************************************************************************
 Function:	Container
 Purpose:	Create a container <div>
