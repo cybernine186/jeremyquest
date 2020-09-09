1bbeac244ac4dfb88ce4b5ae49b5921934ad042a
quido <hiquido@gmail.com>
2020-09-09 00:07:34 -0400

    fix

--- dropped.php
+++ dropped.php
@@ -41,7 +41,7 @@ elseif ($_GET['a'] == "d")
 		data_error();
 	
 	$drop_id = $_GET['id'];
-	$query = "SELECT DATE_FORMAT(time, '%a %b %d, %Y %T') AS thetime, char_id, character_data.name AS name, pickup, zone.short_name, x, y, z FROM qs_player_drop_record JOIN character_data ON character_data.id = qs_player_drop_record.char_id JOIN zone ON zone.zoneidnumber = qs_player_drop_record.zone_id WHERE drop_id = {$drop_id}";
+	$query = "SELECT DATE_FORMAT(time, '%a %b %d, %Y %T') AS thetime, char_id, character_data.name AS name, pickup, zone.short_name, qs_player_drop_record.x AS x, qs_player_drop_record.y AS y, qs_player_drop_record.z AS z FROM qs_player_drop_record JOIN character_data ON character_data.id = qs_player_drop_record.char_id JOIN zone ON zone.zoneidnumber = qs_player_drop_record.zone_id WHERE drop_id = {$drop_id}";
 	$result = $eqdb->query($query);
 	if($result->num_rows < 1)
 		data_error();
