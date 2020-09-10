1aa0ad8790f79d25e6ca2646fbb81181de959f82
quido <hiquido@gmail.com>
2020-09-10 04:33:41 -0400

    fix

--- resetpw.php
+++ resetpw.php
@@ -10,7 +10,7 @@ include_once("functions.php");
 include_once("header.php");
 
 // Check for permissions
-if (!$manage_users)
+if (!$permission_users)
 {
 	RowText("<h5>You are not authorized!</h5>");
 	include_once("footer.php");
