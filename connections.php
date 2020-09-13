c4bf89da4afe9fa588a87bc8511fbed3a4fcb58c
quido <hiquido@gmail.com>
2020-09-13 07:38:10 -0400

    Basic Connections Page
    Plus schema update

--- connections.php
+++ connections.php
@@ -18,6 +18,48 @@ if (!$permission_connections)
 
 RowText("<h4>Connections</h4>");
 
+if (!isset($_GET['a']))
+{
+	display_connection_list($admindb);
+}
+else
+{
+	display_connection_list($admindb);
+}
+
 include_once("footer.php");
+
+function display_connection_list($admindb)
+{
+	$query = "SELECT id, name FROM connections ORDER BY id ASC";
+	$result = $admindb->query($query);
+	if ($result->num_rows < 1)
+	{
+		RowText("No Connections");
+		include_once("footer.php");
+		die;
+	}
+?>
+	<table class="table">
+		<thead>
+			<tr>
+				<th scope="col">ID</th>
+				<th scope="col">Connection Name</th>
+				<th scope="col">Delete</th>
+			</tr>
+		</thead>
+		<tbody>
+<?php
+			while ($row = $result->fetch_assoc())
+			{
+				print "<tr><td>{$row['id']}</td><td>{$row['name']}</td><td>";
+				print "<a class='btn btn-primary' href='connections.php?a=d&id={$row['id']}' role='button'>Delete</a>";
+				print "</td></tr>";
+			}
+		print "</tbody>";
+	print "</table>";
+	
+	print "<a class='btn btn-primary' href='connections.php?a=c' role='button'>Create New</a>";
+}
 
 ?>
