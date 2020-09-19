8ca3882f589347d7ec1986c1e9335d01dcb2c21f
quido <hiquido@gmail.com>
2020-09-19 03:09:18 -0400

    Copy Char Script Base

---
+++ copychar.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("functions.php");
+include_once("header.php");
+
+// Check for permissions
+if (!$permission_copychar)
+{
+	RowText("<h5>You are not authorized!</h5>");
+	include_once("footer.php");
+	die;
+}
+
+RowText("<h4>Copy Character</h4>");
+
+if (!isset($_GET['a']))
+{
+	display_char_search();
+}
+else
+{
+	display_char_search();
+}
+
+include_once("footer.php");
+
+?>
