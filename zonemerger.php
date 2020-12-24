107a0fd9975b1809f67a98df8f9cf70b635ed8ff
quido <hiquido@gmail.com>
2020-12-24 01:26:57 -0500

    Test NPC Deleter

--- zonemerger.php
+++ zonemerger.php
@@ -16,19 +16,36 @@ RowText("<h4>Zone Merger</h4>");
 if (!isset($_GET['a']))
 	display_zoneselect_form($eqdb);
 elseif ($_GET['a'] == "sz")
-	show_zone_tasks($eqdb);
+{
+	if (!IsNumber($_POST['inputZone']))
+		data_error();
+	$zone_id = $_POST['inputZone'];
+	
+	show_zone_tasks($eqdb, $zone_id);
+}
 elseif ($_GET['a'] == "den")
 {
-	delete_existing_npcs($eqdb);
-	show_zone_tasks($eqdb);
+	if (!IsNumber($_GET['zid']))
+		data_error();
+	$zone_id = $_GET['zid'];
+	
+	delete_existing_npcs($eqdb, $zone_id);
+	show_zone_tasks($eqdb, $zone_id);
 }
 else
 	display_zoneselect_form($eqdb);
 
 include_once("footer.php");
 
-function show_zone_tasks($eqdb)
+function show_zone_tasks($eqdb, $zone_id)
 {
+	// Show zone number and name
+	$query = "SELECT short_name FROM zone WHERE zoneidnumber = {$zone_id}";
+	$result = $eqdb->query($query);
+	if ($result->num_rows != 1)
+		data_error();
+	RowText("{$zone_id} - {$row['short_name']}");
+	
 	Row();
 		Col();
 		DivC();
@@ -44,7 +61,7 @@ function show_zone_tasks($eqdb)
 				<tbody>
 					<tr>
 						<td>Delete Existing NPCs</td>
-						<td><a class="btn btn-primary" href="zonemerger.php?a=den" role="button">Go</a></td>
+						<td><a class="btn btn-primary" href="zonemerger.php?a=den&zid=<?php print $zone_id; ?>" role="button">Go</a></td>
 					</tr>
 				</tbody>
 			</table>
@@ -55,9 +72,14 @@ function show_zone_tasks($eqdb)
 	DivC();
 }
 
-function delete_existing_npcs($eqdb)
+function delete_existing_npcs($eqdb, $zone_id)
 {
-	RowText("NPCs deleted...<br />");
+	$npc_id_min = $zone_id * 1000;
+	$npc_id_max = ($zone_id + 1) * 1000;
+	$query = "DELETE FROM npc_types WHERE id >= {$npc_id_min} AND id < {$npc_id_max}";
+	$result = $eqbd->query($query);
+	$affected_rows = $eqdb->affected_rows;
+	RowText("{$affected_rows} NPCs were deleted from Zone {$zone_id}");
 }
 
 function display_zoneselect_form($eqdb = NULL)
