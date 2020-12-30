76885bac18a29ce0f7afa6fae3f935c823377c4c
quido <hiquido@gmail.com>
2020-12-30 06:40:40 -0500

    fix

--- zonemerger.php
+++ zonemerger.php
@@ -893,7 +893,7 @@ function copy_npc_spell_data($eqdb, $wfhdb, $zone_id)
 		}
 		if (!$result_spells->num_rows < 1)
 		{
-			RowText("npc_spells row not found - setting to 0 etc");
+			RowText("npc_spells row {$r['npc_spells_id']} not found - setting to 0 etc");
 			$nsi[$r['npc_spells_id']] = 0;
 			continue;
 		}
