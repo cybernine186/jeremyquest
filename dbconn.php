aefe0c1d1bc7f394337f8b4e3206bd767a8c2720
quido <hiquido@gmail.com>
2021-07-18 01:24:41 -0400

    db connection script update
    one connection moved to header (for navbar)

--- dbconn.php
+++ dbconn.php
@@ -1,24 +1,14 @@
 <?php
+
 include_once("dbcredential.php");
-	$admindb = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-	if ($admindb->connect_errno)
-	{
-		print "Failed to connect to database.";
-		include_once("footer.php");
-		die;
-	}
-	
-	/*
-	
-	$eqdb = new mysqli($eqdbhost, $eqdbuser, $eqdbpass, $eqdbname);
+$admindb = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
-	if ($eqdb->connect_errno)
-	{
-		print "Failed to connect to database.";
-		include_once("footer.php");
-		die;
-	}
-	*/
+if ($admindb->connect_errno)
+{
+	print "Failed to connect to database.";
+	include_once("footer.php");
+	die;
+}
 	
 ?>
